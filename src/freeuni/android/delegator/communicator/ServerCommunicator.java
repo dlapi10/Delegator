8014ece2466b26e7bd225a43f211974800371898
Portgas-D-Ace <dlapiashvili@deltasystems.ge>
2014-07-19 20:19:36 +0400

    ServerCommunicator Created

---
+++ src/freeuni/android/delegator/communicator/ServerCommunicator.java
@@ -0,0 +1,119 @@
+package freeuni.android.delegator.communicator;
+
+import java.util.List;
+
+import freeuni.android.delegator.model.Group;
+import freeuni.android.delegator.model.Task;
+import freeuni.android.delegator.model.User;
+
+public class ServerCommunicator implements DatabaseCommunicator{
+
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String updateUser(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User getUser(String userName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int addGroup(Group group, User user) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void updateGroupName(Group group) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addUserToGroup(Group group, User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Group getGroup(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Group> getUsersGroups(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setSubortinate(User manager, User subordinate) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<User> getSubordinatesForManager(User manager) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int addTask(Task task) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void updateTask(Task task) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteTask(Task task) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Task getTask(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Task> getTasksForReporter(User reporter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Task> getTasksForAssignee(User assignee) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getCurrentTaskCountForUser(User user) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
