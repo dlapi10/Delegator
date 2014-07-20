cbd4269a7d83aeb055bef90888d137d6b5892d79
Ruska <rberi10@freeuni.edu.ge>
2014-07-20 10:13:14 +0400

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit ab76d796a85f9b7f5e2d5107c2006f28a30406e2.

--- src/freeuni/android/delegator/communicator/DatabaseCommunicatorDB.java
+++ src/freeuni/android/delegator/communicator/DatabaseCommunicatorDB.java
@@ -104,4 +104,9 @@ public class DatabaseCommunicatorDB implements DatabaseCommunicator{
 		return db.getCurrentTaskCountForUser(user);
 	}
 
+	@Override
+	public Calendar DateToCalendar(Date date) {
+		return db.DateToCalendar(date);
+	}
+
 }
